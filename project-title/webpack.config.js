bfc63b6f70167a0a5d332089f6a68cc65cec719c
Jason Barone <jason@jasonbarone.com>
2016-03-24 11:15:06 -0400

    Refactor, move stuff around.

---
+++ project-title/webpack.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+
+    resolve: {
+        root: __dirname
+    },
+
+
+    entry: {
+        app: "./js/src/app.js"
+    },
+
+
+    output: {
+        path: "./sqs_template/scripts/",
+        filename: "app.js"
+    },
+
+
+    module: {
+        loaders: [
+            {
+                test: /js\/src\/jquery\/dist.*\.js$/,
+                loader: "expose?$!expose?jQuery"
+            },
+            {
+                test: /js\/src\/.*\.js$/,
+                exclude: [
+                    /node_modules/,
+                    /js\/lib/
+                ],
+                loader: "babel",
+                query: {
+                    // https://github.com/babel/babel-loader#options
+                    // cacheDirectory: true,
+                    presets: [
+                        "es2015",
+                        "stage-0"
+                    ],
+                    plugins: [
+                        "add-module-exports"
+                    ]
+                }
+            }
+        ]
+    }
+};
